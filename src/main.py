1175c8f68a2db25b15584e42829ba4d4b16df284
Ben Wright <ben31w@gmail.com>
2022-03-15 15:39:25 -0400

    reorder functions

--- src/main.py
+++ src/main.py
@@ -21,6 +21,80 @@ from linkObj import linkObj
 from nodeObj import nodeObj
 from requestObj import requestObj
 
+def allocate_bandwidth(path, bandwidth_cost, links):
+    """
+    Given a two-node path a list of links connecting nodes, allocate
+    bandwidth the links specified in the path.
+
+    :param path: a two-node path on the graph (e.g., [7, 5])
+    :param bandwidth_cost: (int) the bandwidth that must be allocated from the
+        link connecting the nodes
+    :param links: a list of the graph's linkObj objects
+    :return: True if the link has enough bandwidth to support this connection;
+        False otherwise
+    """
+    for link in links:
+        if link.linkSrc == path[0] and link.linkDest == path[1] or \
+                link.linkSrc == path[1] and link.linkDest == path[0] and \
+                link.linkBW >= bandwidth_cost:
+            return True
+    return False
+
+
+def connect_nodes(src, dest, resource_cost):
+    """
+    Establish a connection between src and dest nodes.
+    Return True if a connection is established; False otherwise.
+
+    :param src: a nodeObj where the connection starts
+    :param dest: a nodeObj where the connection ends
+    :param resource_cost: (int) the resources that must be allocated from src and dest
+    :return: True if a connection is established; False otherwise.
+    """
+    if src.nodeResources >= resource_cost and dest.nodeResources >= resource_cost:
+        src.nodeResources -= resource_cost
+        dest.nodeResources -= resource_cost
+        return True
+    return False
+
+
+def filter_requests(requests, nodes, links, graph):
+    """
+    Read a list of requests and filter out the invalid ones.
+    For a request to be valid, there must be:
+     1) a valid path between the request's src and dest
+     2) src and dest nodes must have enough resources for the request's cost,
+        and the links between the nodes must have enough bandwidth.
+
+    :param requests: list of requestObj objects to filter
+    :param nodes: nodeObj list (resources will be allocated)
+    :param links: linkObj list (bandwidth will be allocated)
+    :param graph: nx graph
+    :return: a list of valid requests
+    """
+    valid_requests = []
+    DEFAULT_BANDWIDTH_COST = 5
+    for request in requests:
+        valid_connection = True
+        paths = list(nx.shortest_simple_paths(graph, request.src, request.dest))
+        for path in paths:
+            sub_paths = get_sub_paths(path)
+            for sub_path in sub_paths:
+                enough_bandwidth = allocate_bandwidth(sub_path, DEFAULT_BANDWIDTH_COST, links)
+                if not enough_bandwidth:
+                    valid_connection = False
+                    break
+            if not valid_connection:
+                break
+            successful_connection = connect_nodes(nodes[request.src - 1], nodes[request.dest - 1],
+                    request.resource_requirement)
+            if successful_connection:
+                # break once a successful connection is established
+                # (no need to search other paths once this request is filled)
+                valid_requests.append(request)
+                break
+    return valid_requests
+
 
 def get_nodes():
     """Create node objects from input file."""
@@ -73,80 +147,6 @@ def get_sub_paths(path):
         link = [path[i], path[i+1]]
         sub_paths.append(link)
     return sub_paths
-
-
-def filter_requests(requests, nodes, links, graph):
-    """
-    Read a list of requests and filter out the invalid ones.
-    For a request to be valid, there must be:
-     1) a valid path between the request's src and dest
-     2) src and dest nodes must have enough resources for the request's cost,
-        and the links between the nodes must have enough bandwidth.
-
-    :param requests: list of requestObj objects to filter
-    :param nodes: list of nodeObj objects (resources will be allocated)
-    :param links: list of linkObj objects (bandwidth will be allocated)
-    :return: a list of valid requests
-    """
-    valid_requests = []
-    DEFAULT_BANDWIDTH_COST = 5
-    for request in requests:
-        valid_connection = True
-        paths = list(nx.shortest_simple_paths(graph, request.src, request.dest))
-        for path in paths:
-            sub_paths = get_sub_paths(path)
-            for sub_path in sub_paths:
-                enough_bandwidth = allocate_bandwidth(sub_path, DEFAULT_BANDWIDTH_COST, links)
-                if not enough_bandwidth:
-                    valid_connection = False
-                    break
-            if not valid_connection:
-                break
-            successful_connection = connect_nodes(nodes[request.src - 1], nodes[request.dest - 1],
-                    request.resource_requirement)
-            if successful_connection:
-                # break once a successful connection is established
-                # (no need to search other paths once this request is filled)
-                valid_requests.append(request)
-                break
-    return valid_requests
-
-
-def connect_nodes(src, dest, resource_cost):
-    """
-    Establish a connection between src and dest nodes.
-    Return True if a connection is established; False otherwise.
-
-    :param src: a nodeObj where the connection starts
-    :param dest: a nodeObj where the connection ends
-    :param resource_cost: (int) the resources that must be allocated from src and dest
-    :return: True if a connection is established; False otherwise.
-    """
-    if src.nodeResources >= resource_cost and dest.nodeResources >= resource_cost:
-        src.nodeResources -= resource_cost
-        dest.nodeResources -= resource_cost
-        return True
-    return False
-
-
-def allocate_bandwidth(path, bandwidth_cost, links):
-    """
-    Given a two-node path a list of links connecting nodes, allocate
-    bandwidth the links specified in the path.
-
-    :param path: a two-node path on the graph (e.g., [7, 5])
-    :param bandwidth_cost: (int) the bandwidth that must be allocated from the
-        link connecting the nodes
-    :param links: a list of the graph's linkObj objects
-    :return: True if the link has enough bandwidth to support this connection;
-        False otherwise
-    """
-    for link in links:
-        if link.linkSrc == path[0] and link.linkDest == path[1] or \
-                link.linkSrc == path[1] and link.linkDest == path[0] and \
-                link.linkBW >= bandwidth_cost:
-            return True
-    return False
 
 
 if __name__ == '__main__':
